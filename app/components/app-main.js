3b47e16d96698202d173462f051c28dd080f228d
ubuntuvim <1527254027@qq.com>
2018-05-06 21:33:17 +0800

    complete static page..

--- app/components/app-main.js
+++ app/components/app-main.js
@@ -1,38 +1,49 @@
 import Component from '@ember/component';
-import { inject as service } from '@ember/service';
-import markdownToHTML from '../utils/markdown-to-html';
+// import { inject as service } from '@ember/service';
+// import markdownToHTML from '../utils/markdown-to-html';
 
 export default Component.extend({
     fileName: null,
     didInsertElement() {
+
         let e = editormd("editormd", {
+            taskList : true,
+            tocm            : true,         // Using [TOCM]
+            tex : true,                   // 开启科学公式TeX语言支持，默认关闭
+            flowChart : true,             // 开启流程图支持，默认关闭
+            sequenceDiagram : true,       // 开启时序/序列图支持，默认关闭,
+            toolbarAutoFixed: false,
+            toolbarIcons : function() {
+                return [
+                    "bold", "del", "italic", "quote", "|",
+                    "h1", "h2", "h3", "h4", "h5", "h6", "|",
+                    "list-ul", "list-ol", "hr", "|",
+                    "watch"
+                    // "link", "imageUpload", "code", "table", "emoji", "|",
+                    // "insertLink", "imageUpload", "|",
+                    // "watch", "preview", "|"  //"fullscreen",
+                    // "help", "info", "|",
+                    // "returnPreLevel", "|",
+                    // "autosave"
+                ];
+            },
             syncScrolling : "single",
             path : "/assets/editormd/lib/" // Autoload modules mode, codemirror, marked... dependents libs path
         });
-        e.hide();
+        // e.hide();
         window.markdownEditor = e;
     },
     actions: {
         showMarkdownContent(mdFileName) {
+            // window.markdownEditor.show();
             this.set("fileName", mdFileName);
             let url = `/assets/json-data/${mdFileName}.md`;
             //动态设置编辑器的内容
-            $.get(url, function(data) {
-                Ember.$("#editormd-view").empty();  //先清空原有记录
-                markdownToHTML("editormd-view", {markdown : data});
+            Ember.$.get(url, function(data) {
+                // Ember.$("#editormd").empty();  //先清空原有记录
+                window.markdownEditor.setValue(data);
 			});
 
-        },
-        // 双击预览页面转到编辑模式
-        edit(fileName) {
-            console.log('fileName == ' + fileName);
-            //动态设置编辑器的内容
-            let url = `/assets/json-data/${fileName}.md`;
-            $.get(url, function(data) {
-                // Ember.$("#editormd-view").hide();
-                window.markdownEditor.setValue(data);
-                window.markdownEditor.show();
-			});
         }
     }
 });
